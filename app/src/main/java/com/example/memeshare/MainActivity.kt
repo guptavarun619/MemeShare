98eba0c4c9a1b5c5bb88831997a599b6778fc108
Varun Vaibhav <guptavarun619@gmail.com>
2020-11-06 00:00:32 +0530

    API request

--- app/src/main/java/com/example/memeshare/MainActivity.kt
+++ app/src/main/java/com/example/memeshare/MainActivity.kt
@@ -3,13 +3,47 @@ package com.example.memeshare
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadMeme()
+    }
+
+    private fun loadMeme() {
+        // Instantiate the RequestQueue.
+        
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://meme-api.herokuapp.com/gimme"
+
+        // Request a string response from the provided URL.
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            { response ->
+                val url = response.getString("url")
+                Glide.with(this).load(url).into(memeImageView)
+            },
+            Response.ErrorListener{
+                Toast.makeText(this,"Something went wrong", Toast.LENGTH_LONG).show()
+            }
+        )
+
+
+        // Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest)
+
     }
 
     fun nextMeme(view: View) {}
-    fun shareMeme(view: View) {}
+    fun shareMeme(view: View) {
+        loadMeme()
+    }
 }
